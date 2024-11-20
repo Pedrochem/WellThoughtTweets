6c0b5dbce19fe8d675ffabf49b432ab99debb483
Pedro Chem <pedrochem@hotmail.com>
2024-11-19 22:08:25 -0300

    quit popup after save

--- popup.js
+++ popup.js
@@ -226,6 +226,7 @@ saveButton.addEventListener('click', () => {
       criteriaManager.checkForChanges();
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.reload(tabs[0].id);
+        window.close();
       });
     });
   });
